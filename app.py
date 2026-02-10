6c4cc496367358b941fbebd7df199dfda83f98c0
gryuewua <yuchenw7@nycstudents.net>
2026-02-10 09:12:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-item = {
-    "name": "N/A",
-    "price": "N/A",
-    "description": "N/A",
+bakery_items = {
 
+    "name": "cookies",
+    "price": "11",
+    "amount": "11",
+    "description": "Hard as concrete, tastes like concrete as well: do NOT use 'I can't believe it's not butter!'."
+    
+    "name" "brownie",
+    "price": "8",
+    "amount": "4",
+    "description": "Delectable treat, creation process is questionable.",
 
-
-
-
-
-
-
+    "name":  "cake",
+    "price": "25",
+    "amount": "1",
+    "description": "Hopefully not as bad as those cookies."
 }
+
+print(bakery_items[1]["name"])
+
