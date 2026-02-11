b5f64b94bc542f266ea95a25416bd497331e8c15
gryuewua <yuchenw7@nycstudents.net>
2026-02-11 09:26:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-bakery_items = {
-
+bakery_items = [
+    {
     "name": "cookies",
     "price": "11",
     "amount": "11",
     "description": "Hard as concrete, tastes like concrete as well: do NOT use 'I can't believe it's not butter!'."
-    
-    "name" "brownie",
-    "price": "8",
-    "amount": "4",
-    "description": "Delectable treat, creation process is questionable.",
-
-    "name":  "cake",
+    },
+    {"name":  "cake",
+    "price": "50",
+    "amount": "1",
+    "description": "Hopefully not as bad as those cookies, but that's wishful thinking."
+    },
+    {"name":  "",
     "price": "25",
     "amount": "1",
-    "description": "Hopefully not as bad as those cookies."
-}
+    "description": "Hopefully not as bad as those cookies, but that's wishful thinking."
+    }
+]
 
-print(bakery_items[1]["name"])
+for items in bakery_items:
+    if ["name"] == "cake":
+        print(bakery_items[0] ["name"])
 
