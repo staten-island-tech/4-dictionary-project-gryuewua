c2aaf4f874522e249fe0daa20af007ac7354380c
gryuewua <yuchenw7@nycstudents.net>
2026-02-12 09:24:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,70 @@
 bakery_items = [
     {
     "name": "cookies",
-    "price": "11",
-    "amount": "11",
+    "price": int("11"),
     "description": "Hard as concrete, tastes like concrete as well: do NOT use 'I can't believe it's not butter!'."
     },
     {"name":  "cake",
-    "price": "50",
-    "amount": "1",
+    "price": int("50"),
     "description": "Hopefully not as bad as those cookies, but that's wishful thinking."
     },
-    {"name":  "",
-    "price": "25",
-    "amount": "1",
-    "description": "Hopefully not as bad as those cookies, but that's wishful thinking."
+    {"name":  "pboy",
+    "price": int("1"),
+    "description": "placeholder"
     }
 ]
 
-for items in bakery_items:
-    if ["name"] == "cake":
-        print(bakery_items[0] ["name"])
+for index, item in enumerate(bakery_items):
+    print(index, ":", item["name"])
 
+purchase = int(input("What would you like to buy?"))  
+
+receipt = []
+
+total = []
+
+'''if purchase == 0:
+    print(bakery_items[0]["name"])
+    confirm = input("Are you sure?")
+    if confirm == "yes":
+        receipt.append(bakery_items[0]["name"])
+        total.append(bakery_items[0]["price"])
+    else:
+        checkout = (input("Would you like to checkout"))  
+        if checkout == "no":
+            purchase = int(input("What would you like to buy?"))  
+        elif checkout == "yes":
+            end = input("Thank you for shopping with us! Would you like a reciept?")
+            if end == "yes":
+                print (receipt)
+                print (f"Your total is ${sum(total)}")
+elif purchase == 1:
+    print(bakery_items[1]["name"])
+    confirm = input("Are you sure?")
+    if confirm == "yes":
+        receipt.append(bakery_items[1]["name"])
+        total.append(bakery_items[1]["price"])
+    else:
+        checkout = (input("Would you like to checkout"))  
+        if checkout == "no":
+            purchase = int(input("What would you like to buy?"))  
+        elif checkout == "yes":
+            end = input("Thank you for shopping with us! Would you like a reciept?")
+            if end == "yes":
+                print (receipt)
+                print (f"Your total is ${sum(total)}")
+elif purchase == 2:
+    print(bakery_items[2]["name"])
+    confirm = input("Are you sure?")
+    if confirm == "yes":
+        receipt.append(bakery_items[2]["name"])
+        total.append(bakery_items[2]["price"])
+    else:
+        checkout = (input("Would you like to checkout"))  
+        if checkout == "no":
+            purchase = int(input("What would you like to buy?"))  
+        elif checkout == "yes":
+            end = input("Thank you for shopping with us! Would you like a reciept?")
+            if end == "yes":
+                print (receipt)
+                print (f"Your total is ${sum(total)}") '''
